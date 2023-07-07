f7e5ed631044002a6d1d8dd648b129eadf65aa79
Maxim Korchagin <Airfallsc2@gmail.com>
2023-07-07 11:57:10 +0300

    init commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        List<Integer> sortedList = new ArrayList<>();
+
+        for (Integer list : intList) {
+            if (list > 0 && list % 2 == 0) {
+                sortedList.add(list);
+            }
+        }
+        Collections.sort(sortedList);
+        System.out.println(sortedList);
+
+        Stream<Integer> stream = intList.stream();
+        stream.filter(x -> x > 0)
+                .filter(x -> x % 2 == 0)
+                .sorted(Comparator.naturalOrder())
+                .forEach(System.out::println);
+    }
+}
